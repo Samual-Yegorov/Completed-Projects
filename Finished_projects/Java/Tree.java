abc97dfa0a7847c2075031573af49d53e5032bec
Samual Yegorov <71674857+Samual-Yegorov@users.noreply.github.com>
2022-02-25 16:33:16 -0800

    yayeaa

---
+++ Finished_projects/Java/Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+
+public class Tree {
+  public static void main(String[] args) {
+  System.out.println("Hey there, i'm Sam and I'm learning to code in Jave!");
+  System.out.println("  *  ");
+  System.out.println(" *** ");
+  System.out.println(" *** ");
+  System.out.println("  *  ");
+  System.out.println("  *  ");
+}
+
+}
