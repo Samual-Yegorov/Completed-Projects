a19334d2a66b5644cae565a3b642bf3aaaaf95f0
Samual Yegorov <71674857+Samual-Yegorov@users.noreply.github.com>
2022-04-05 15:37:26 -0700

    commenting

--- Finished_projects/Python3.0/CourseAcademyP/getting-rdy-for-physics-class.py
+++ Finished_projects/Python3.0/CourseAcademyP/getting-rdy-for-physics-class.py
@@ -1,4 +1,4 @@
-You are a physics teacher preparing for the upcoming semester. You want to provide your students with some functions that will help them calculate some fundamental physical properties.
+#You are a physics teacher preparing for the upcoming semester. You want to provide your students with some functions that will help them calculate some fundamental physical properties.
 
 train_mass = 22680
 train_acceleration = 10
@@ -7,6 +7,7 @@ bomb_mass = 1
 
 
 # Write your code below: 
+# These are simple transition practices for f to celsius
 def f_to_c(f_temp):
   return (f_temp - 32) * 5/9
 
@@ -19,6 +20,8 @@ def c_to_f(c_temp):
 c0_in_fahrenheit = c_to_f(0)
 print(c0_in_fahrenheit) 
 
+#this is now practice using a defined function 
+
 def get_force(mass, acceleration):
   return mass * acceleration
 train_force = get_force(train_mass, train_acceleration)
@@ -29,6 +32,8 @@ def get_energy(mass, c=3*10**8):
   return mass * c**2
 bomb_energy = get_energy(bomb_mass)
 print("A 1kg bomb supplies " + str(bomb_energy) + " Joules.")
+
+#this function show the amount of a joules a train will work over 100 meters
 
 def get_work(mass, acceleration, distance):
   get_force = mass * acceleration
